c1cb28da47d9be28b327d5f780d423c80fd1e438
JandersonCRB <jal@ic.ufal.br>
2018-03-10 20:52:33 -0300

    fixed drawer in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,20 +6,23 @@ import IconButton from 'material-ui/IconButton';
 import Toolbar from 'material-ui/Toolbar';
 import Drawer from 'material-ui/Drawer';
 import MenuIcon from 'material-ui-icons/Menu';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import DraftsIcon from 'material-ui-icons/Drafts';
 
 class MyNavbar extends React.Component {
-	constructor(){
+	constructor() {
 		super();
 		this.state = {
 			drawer: false
 		}
 	}
 	render() {
+		console.log(this.state);
 		return (
 			<React.Fragment>
 				<AppBar position="static">
 					<Toolbar>
-						<IconButton color="secondary" style={{ marginLeft: -12, marginRight: 20 }} aria-label="Menu">
+						<IconButton color="secondary" style={{ marginLeft: -12, marginRight: 20 }} aria-label="Menu" onClick={() => this.setState({drawer: true})}>
 							<MenuIcon />
 						</IconButton>
 						<h1>
@@ -28,13 +31,21 @@ class MyNavbar extends React.Component {
 						<Button style={{ marginLeft: 'auto' }} color="secondary">Login</Button>
 					</Toolbar>
 				</AppBar>
-				<Drawer open={this.state.left} onClose={this.setState({drawer: false})}>
+				<Drawer open={this.state.drawer}>
 					<div
 						tabIndex={0}
 						role="button"
-						onClick={this.setState({drawer: false})}
-						onKeyDown={this.setState({drawer: false})}
+					onClick={() => this.setState({drawer: false})}
+					onKeyDown={() => this.setState({drawer: false})}
 					>
+						<List>
+							<ListItem button>
+								<ListItemIcon>
+									<DraftsIcon />
+								</ListItemIcon>
+								<ListItemText primary="Drafts" />
+							</ListItem>
+						</List>
 					</div>
 				</Drawer>
 			</React.Fragment>
