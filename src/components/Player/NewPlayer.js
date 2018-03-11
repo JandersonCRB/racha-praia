08f153ddce9d44010dd66be3e350a676e8bbe1a1
JandersonCRB <jal@ic.ufal.br>
2018-03-10 22:23:41 -0300

    Fixed bugs in New Player

--- src/components/Player/NewPlayer.js
+++ src/components/Player/NewPlayer.js
@@ -28,12 +28,9 @@ export default class NewPlayer extends React.Component {
 		e.preventDefault();
 
 		this.props.players.add({
-			fullname: this.refs.fullname.value,
-			nickname: this.refs.nickname.value,
+			fullname: this.state.fullname,
+			nickname: 'x'
 		});
-
-		this.refs.fullname.value = null;
-		this.refs.nickname.value = null;
 	}
 	render() {
 		return (
@@ -43,7 +40,7 @@ export default class NewPlayer extends React.Component {
 				<Paper evelation={2} style={{ padding: 10, maxWidth: '278px', marginLeft: 'auto', marginRight: 'auto' }}>
 					<form onSubmit={this.addContact}>
 						<fieldset>
-							<TextField name='nickname' label='Nome' value={this.state.fullname} style={styles.fields} fullWidth />
+							<TextField name='nickname' label='Nome' onChange={fullname => {this.setState({fullname: fullname.target.value})}} value={this.state.fullname} style={styles.fields} fullWidth />
 							<Button type="submit" variant="raised" color="primary" fullWidth>Enviar</Button>
 						</fieldset>
 					</form>
